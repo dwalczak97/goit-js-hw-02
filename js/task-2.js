777df22413a0f674367008570dccbe0cb25ef060
dwalczak97 <d.melon@op.pl>
2024-05-21 09:18:32 +0200

    task 2 poprawki

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,11 @@
 function formatMessage(message, maxLength) {
-  if (message <= maxLength) {
+  if (message.length <= maxLength) {
     return message;
   } else {
-    return message.slice(0, maxLength).trim() + `...`;
+    return message.slice(0, maxLength).trim() + "...";
   }
 }
+
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
